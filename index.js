d1ade87012ba8b34771c7d8eda8676f4451e2dac
sharan2717 <sharan2717@gmail.com>
2024-08-30 17:03:07 +0530

    g

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
